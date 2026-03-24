59a3c04199f935f5fd85c75be935b8e74977ad5f
python-goodfans <pythonfans@outlook.com>
2026-03-24 14:33:41 +0800

    Create WSGI application entry point

---
+++ backend/music_platform/wsgi.py
@@ -0,0 +1,9 @@
+# WSGI Application Entry Point
+
+import os
+from django.core.wsgi import get_wsgi_application
+
+# Specify the settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+
+application = get_wsgi_application()
