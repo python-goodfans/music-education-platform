9c78e7bc4fe0693bc1e750773823eadc6128f5fc
python-goodfans <pythonfans@outlook.com>
2026-03-24 10:31:04 +0800

    Create CardCarousel.vue component

---
+++ src/components/CardCarousel.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="carousel">
+    <div v-for="item in items" :key="item.id" class="carousel-item">
+      <img :src="item.image" :alt="item.title" />
+      <h3>{{ item.title }}</h3>
+      <p>{{ item.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [] // This will hold your carousel items
+    };
+  },
+  created() {
+    // Fetch or initialize your carousel items here
+  }
+};
+</script>
+
+<style scoped>
+.carousel {
+  display: flex;
+  overflow-x: auto;
+}
+.carousel-item {
+  min-width: 200px;
+  margin: 10px;
+}
+</style>
