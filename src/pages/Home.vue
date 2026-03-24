c49c7c87fa1c7a8d2c99ef628283c27e1a9916e3
python-goodfans <pythonfans@outlook.com>
2026-03-24 14:32:20 +0800

    Create Home.vue for home page component

---
+++ src/pages/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Banner />
+    <CardCarousel />
+    <div class="features">
+      <h2>Features</h2>
+      <ul>
+        <li>Feature 1</li>
+        <li>Feature 2</li>
+        <li>Feature 3</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import Banner from '@/components/Banner.vue';
+import CardCarousel from '@/components/CardCarousel.vue';
+
+export default {
+  components: {
+    Banner,
+    CardCarousel,
+  },
+};
+</script>
+
+<style scoped>
+.features {
+  margin-top: 20px;
+}
+</style>
